05cd161d94923addc654420f4ae90110dc05e92d
MRALADAG <mstf_aladag@yahoo.com>
2022-09-17 23:48:13 +0300

    performed extra String operations

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,26 @@ public class Main {
         System.out.println(temp);
         System.out.println("Bugün kelimesinin başlangıç index'i : " + mesaj.indexOf("Bugün"));
         System.out.println(mesaj.lastIndexOf("a"));
+
+        System.out.println(mesaj.replace(' ', '-'));
+        String yeniMesaj = mesaj.replace(' ', '-');
+        System.out.println(mesaj.substring(6));
+        System.out.println(mesaj.substring(6, 11));
+
+        System.out.println("\nsplit method is started.\n");
+        for (String kelime : mesaj.split(" ")) {
+            // * Bu metod String array'i döndürüyor.
+            System.out.println(kelime);
+        }
+
+        System.out.println("***************************************************************");
+        System.out.println("All of the letter of mesaj statement is converted lovercase\n");
+        System.out.println(mesaj.toLowerCase());
+
+        System.out.println("\nAll of the letter of mesaj statement is converted uppercase\n");
+        System.out.println(mesaj.toUpperCase());
+
+        System.out.println(mesaj.trim());
+
     }
 }
